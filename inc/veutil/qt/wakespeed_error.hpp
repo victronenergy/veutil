1f7546d01b81f46cd845a0196c4c4bd6c4a26845
Bea Lam <bea.lam@qinetic.com.au>
2024-01-16 11:12:25 +1000

    Define Bms, Charger and Wakespeed error classes as QML singleton types
    Add QML_ELEMENT and QML_SINGLETON to BmsError, ChargerError and
    WakespeedError to allow them to be used as singletons in QML without
    needing to use qmlRegisterSingletonType().

--- inc/veutil/qt/wakespeed_error.hpp
+++ inc/veutil/qt/wakespeed_error.hpp
@@ -3,9 +3,17 @@
 #include <QObject>
 #include <QString>
 
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#include <qqmlintegration.h>
+#endif
+
 class WakespeedError : public QObject
 {
 	Q_OBJECT
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+	QML_ELEMENT
+	QML_SINGLETON
+#endif
 
 public:
 	Q_INVOKABLE QString description(int error) { return WakespeedError::getDescription(error); }

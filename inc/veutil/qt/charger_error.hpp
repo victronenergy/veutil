517d533b1c01e23eae8277bee9b7a6770b79fbf8
Bea Lam <bea.lam@qinetic.com.au>
2024-02-28 13:02:50 +1000

    Define Bms, Charger and Alternator error classes as QML singleton types
    Add QML_ELEMENT and QML_SINGLETON to BmsError, ChargerError and
    AlternatorError to allow them to be used as singletons in QML without
    needing to use qmlRegisterSingletonType().

--- inc/veutil/qt/charger_error.hpp
+++ inc/veutil/qt/charger_error.hpp
@@ -2,6 +2,10 @@
 
 #include <QObject>
 
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#include <qqmlintegration.h>
+#endif
+
 typedef struct
 {
 	int errorId;
@@ -11,6 +15,11 @@ typedef struct
 class ChargerError : public QObject
 {
 	Q_OBJECT
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+	QML_ELEMENT
+	QML_SINGLETON
+#endif
+
 public:
 
 	Q_INVOKABLE QString description(int error) { return ChargerError::getDescription(error); }

dca54cd85c88bc17655f7668d2e769875b92b09e
Martin Bosma <mbosma@victronenergy.com>
2024-03-19 12:09:05 +0100

    GensetError: add Hatz failure codes

--- src/qt/genset_error.cpp
+++ src/qt/genset_error.cpp
@@ -14,12 +14,29 @@ private:
 	FischerPanda() = default;
 };
 
+class Hatz
+{
+	Q_DECLARE_TR_FUNCTIONS(Hatz)
+
+public:
+	static QString getDescription(QString errorId);
+
+	static constexpr quint32 SpnAndFmiToNumber(quint32 spn, quint8 fmi) {
+		return spn << 8 | fmi;
+	}
+
+private:
+	Hatz() = default;
+};
+
 QString GensetError::getDescription(QString errorId, int nrOfPhases)
 {
 	if (errorId == "")
 		return tr("No error");
 	if (errorId.startsWith("fischerpanda:", Qt::CaseInsensitive))
 		return FischerPanda::getDescription(errorId, nrOfPhases);
+	if (errorId.startsWith("hatz:", Qt::CaseInsensitive))
+		return Hatz::getDescription(errorId);
 	return tr("Unknown error: %1").arg(errorId);
 }
 
@@ -142,6 +159,67 @@ QString FischerPanda::getDescription(QString errorId, int nrOfPhases)
 		case 165: result += tr("Emergency stop"); break;
 		case 166: result += tr("No connection"); break;
 		default: result += tr("Unknown error"); break;
+	}
+
+	return result;
+}
+
+QString Hatz::getDescription(QString errorId)
+{
+	const auto dashIdx = errorId.indexOf("-");
+	if (dashIdx < 1)
+		return tr("Unknown error: ") + errorId;
+
+	errorId.remove(0, dashIdx + 1);
+
+	const auto parts = errorId.split('.');
+	if (parts.size() != 2)
+		return tr("Unknown error: ") + errorId;
+
+	bool ok[2];
+	const auto spn = parts[0].toUInt(&ok[0]);
+	const auto fmi = parts[1].toUInt(&ok[1]);
+	if (!ok[0] || !ok[1])
+		return tr("Unknown error: ") + errorId;
+
+	auto result = "#" + errorId + " ";
+
+	/* These SPN+FMI fault codes and their descriptions are taken
+	 * from the Hatz 05666801.1823/Fehlerlist V529 document */
+	switch (SpnAndFmiToNumber(spn, fmi)) {
+	case SpnAndFmiToNumber( 100,  7): result += tr("Oil pressure"); break;
+	case SpnAndFmiToNumber( 110,  7): result += tr("Cylinder head overtemperature"); break;
+	case SpnAndFmiToNumber( 167,  7): result += tr("Charge control"); break;
+	case SpnAndFmiToNumber( 190,  1): result += tr("Speed higher than expected"); break;
+	case SpnAndFmiToNumber( 190, 15): result += tr("Overspeed"); break;
+	case SpnAndFmiToNumber( 175, 15): /* Fall-through */
+	case SpnAndFmiToNumber( 175, 16): result += tr("Oiltemperature higher than expected"); break;
+	case SpnAndFmiToNumber( 175,  3): result += tr("Oiltemperature open circuit / short to power"); break;
+	case SpnAndFmiToNumber( 175,  4): result += tr("Oiltemperature short to ground"); break;
+	case SpnAndFmiToNumber(  91,  3): result += tr("Analog setpoint high / short to power"); break;
+	case SpnAndFmiToNumber(  91,  4): result += tr("Analog setpoint low / short to ground"); break;
+	case SpnAndFmiToNumber( 695,  9): result += tr("TSC1 message receive timeout"); break;
+	case SpnAndFmiToNumber( 986, 12): result += tr("CM1 message receive timeout"); break;
+	case SpnAndFmiToNumber( 168, 15): result += tr("Battery voltage high"); break;
+	case SpnAndFmiToNumber( 168, 17): result += tr("Battery voltage low"); break;
+	case SpnAndFmiToNumber( 636,  2): result += tr("Speed signal distorted"); break;
+	case SpnAndFmiToNumber(1079,  3): result += tr("Internal 5V sensor supply high"); break;
+	case SpnAndFmiToNumber(1079,  4): result += tr("Internal 5V sensor supply low"); break;
+	case SpnAndFmiToNumber( 108,  0): result += tr("Barometric pressure high"); break;
+	case SpnAndFmiToNumber( 108,  1): result += tr("Barometric pressure low"); break;
+	case SpnAndFmiToNumber(1347,  3): result += tr("Output fuelpump short to power"); break;
+	case SpnAndFmiToNumber(1347,  5): result += tr("Output fuelpump short to ground"); break;
+	case SpnAndFmiToNumber( 676,  3): result += tr("Output glow plug short to power"); break;
+	case SpnAndFmiToNumber( 676,  5): result += tr("Output glow plug short to ground"); break;
+	case SpnAndFmiToNumber( 651,  5): result += tr("Injector open circuit/low side short to ground"); break;
+	case SpnAndFmiToNumber( 651,  6): result += tr("Injector coil internal short circuit"); break;
+	case SpnAndFmiToNumber( 651,  3): result += tr("Injector low side short to power"); break;
+	case SpnAndFmiToNumber( 915, 14): result += tr("Service hours expired"); break;
+	case SpnAndFmiToNumber( 629, 31): /* Fall-through */
+	case SpnAndFmiToNumber( 630, 12): /* Fall-through */
+	case SpnAndFmiToNumber( 628, 13): /* Fall-through */
+	case SpnAndFmiToNumber(1634,  2): result += tr("Processor failure"); break;
+	default: result += tr("Unknown error"); break;
 	}
 
 	return result;

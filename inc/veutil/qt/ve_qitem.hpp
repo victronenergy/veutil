c71773ed970c95c5bc0b5b15bb6699bded6951c5
Bea Lam <bea.lam@qinetic.com.au>
2025-06-23 09:47:43 +1000

    Add VeQItem::isLeaf property to access isLeaf function from QML

--- inc/veutil/qt/ve_qitem.hpp
+++ inc/veutil/qt/ve_qitem.hpp
@@ -140,6 +140,7 @@ class VE_QITEM_EXPORT VeQItem : public QObject
 	Q_PROPERTY(QString lastValidText READ getLastValidText NOTIFY textChanged)
 	Q_PROPERTY(bool seen READ getSeen NOTIFY seenChanged)
 	Q_PROPERTY(bool sensitive READ getSensitive WRITE setSensitive NOTIFY sensitiveChanged)
+	Q_PROPERTY(bool isLeaf READ isLeaf NOTIFY isLeafChanged)
 	Q_PROPERTY(QString uid READ uniqueId CONSTANT)
 	Q_PROPERTY(QString id READ id CONSTANT)
 
@@ -410,6 +411,7 @@ signals:
 
 	void stateChanged(VeQItem::State state);
 	void textStateChanged(VeQItem::State state);
+	void isLeafChanged();
 
 	/*
 	 * Note: if you want to connect to signals of the childs in a cross thread

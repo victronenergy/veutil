21f47ece2ce32266c4c995e3ba9032c63966983d
Chris Adams <chris.adams@qinetic.com.au>
2025-06-25 10:33:39 +1000

    MQTT: Use clean session connection
    We don't re-use old sessions since we regenerate a unique
    session id upon each connection.
    
    Hence, don't require the broker to keep the old session around
    after connection is lost, otherwise these can accumulate and
    cause increased memory and CPU usage.
    
    (cherry picked from commit a53c45bb60abf4799765948fe981a2eaf1aeec66)

--- src/qt/ve_qitems_mqtt.cpp
+++ src/qt/ve_qitems_mqtt.cpp
@@ -588,7 +588,7 @@ void VeQItemMqttProducer::continueConnect()
 	// race condition where we might have two connections active.
 	QMetaObject::invokeMethod(this, [this] {
 		if (mMqttConnection) {
-			mMqttConnection->setCleanSession(false);
+			mMqttConnection->setCleanSession(true);
 			mMqttConnection->connectToHost();
 		} else {
 			// the MQTT connection was deleted out from under us...

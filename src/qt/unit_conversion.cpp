c69bfeb091b43ea47a100009697ddff78515f257
Nils Citoleux <ncitoleux@victronenergy.com>
2026-07-31 14:37:36 +0200

    Units: fix energy per distance coefficients

--- src/qt/unit_conversion.cpp
+++ src/qt/unit_conversion.cpp
@@ -153,11 +153,11 @@ AltitudeConverter::AltitudeConverter() :
 EnergyPerDistanceConverter::EnergyPerDistanceConverter() :
 	UnitConverter(Unit::WattHourPerKilometre, {
 		{ Unit::WattHourPerKilometre, new UnitBase("Wh/km") },
-		{ Unit::WattHourPerMile, new UnitLinear("Wh/mi", 0.621371) },
-		{ Unit::WattHourPerNauticalMile, new UnitLinear("Wh/NM", 0.539957) },
+		{ Unit::WattHourPerMile, new UnitLinear("Wh/mi", 1/0.621371) },
+		{ Unit::WattHourPerNauticalMile, new UnitLinear("Wh/NM", 1/0.539957) },
 		{ Unit::AmpHourPerKilometre, new UnitBase("Ah/km") },
-		{ Unit::AmpHourPerMile, new UnitLinear("Ah/mi", 0.621371) },
-		{ Unit::AmpHourPerNauticalMile, new UnitLinear("Ah/NM", 0.539957) },
+		{ Unit::AmpHourPerMile, new UnitLinear("Ah/mi", 1/0.621371) },
+		{ Unit::AmpHourPerNauticalMile, new UnitLinear("Ah/NM", 1/0.539957) },
 	}) {}
 
 const UnitConverter *UnitConverters::get(Unit::Type unit)
